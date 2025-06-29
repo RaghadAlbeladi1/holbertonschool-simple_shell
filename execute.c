4d8983d40a6ed85a87d81421399e6a84f74b88d9
“NajwaAljunaidel” <najwa.aljunaidel@gmail.com>
2025-06-29 17:49:32 +0000

    UPDATED THRID TIME

--- execute.c
+++ execute.c
@@ -2,30 +2,42 @@
 
 /**
  * run_command - Forks and executes a command.
- * @args: NULL~@~Qterminated array of arguments.
+ * @args: NULL-terminated array of arguments.
  *
  * Return: Exit status of the executed command.
  */
 int run_command(char **args)
 {
-pid_t pid;
-int   status;
-pid = fork();
-if (pid == 0)
-{
-if (execve(args[0], args, environ) == -1)
-{
-perror("simple_shell");
-exit(EXIT_FAILURE);
+	pid_t pid;
+	int status = 1, child_status;
+
+	/* إذا لا يوجد أمر */
+	if (args[0] == NULL)
+		return (status);
+
+	pid = fork();
+	if (pid == 0)
+	{
+
+		if (execvp(args[0], args) == -1)
+		{
+			perror("simple_shell");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else if (pid < 0)
+	{
+		perror("simple_shell");
+		return (EXIT_FAILURE);
+	}
+	else
+	{
+		if (waitpid(pid, &child_status, 0) == -1)
+			perror("simple_shell");
+		else if (WIFEXITED(child_status))
+			status = WEXITSTATUS(child_status);
+	}
+
+	return (status);
 }
-}
-else if (pid < 0)
-{
-perror("simple_shell");
-}
-else
-{
-waitpid(pid, &status, 0);
-}
-return (status);
-}
+

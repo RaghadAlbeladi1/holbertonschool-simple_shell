5618a03b45a37e0e740b5b6ee709a86e5c2cc5be
“NajwaAljunaidel” <najwa.aljunaidel@gmail.com>
2025-06-30 09:56:06 +0000

    UPDATEDDDDD

--- shell.h
+++ shell.h
@@ -8,18 +8,18 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+
 #define SHELL_RL_BUFSIZE 1024
 #define SHELL_TOK_DELIM  " \t\r\n"
 
 extern char **environ;
+
 
 void print_prompt(void);
 void shell_loop(void);
 char *read_command(void);
 char **split_command(char *command);
 int run_command(char **args);
-
-int handle_builtins(char **args, char **env);
 
 int builtin_env(char **args);
 int builtin_exit(char **args);

6aff2c19bcd6abab5750a1cad52dfed5727ae866
RaghadAlbeladi <Raghadz_5@hotmail.com>
2025-07-01 22:41:10 +0000

    add

--- user_input.c
+++ user_input.c
@@ -1,58 +1,62 @@
-
 #include "shell.h"
 
 /**
- * read_line - Reads user input from stdin.
- * Return: Pointer to input string.
+ * read_line - Reads a line from stdin
+ * Return: Pointer to the line
  */
 char *read_line(void)
 {
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t nread;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
 
-	nread = getline(&line, &len, stdin);
-	if (nread == -1)
-	{
-		free(line);
-		return (NULL);
-	}
-	return (line);
+    nread = getline(&line, &len, stdin);
+    if (nread == -1)
+    {
+        free(line);
+        return NULL;
+    }
+
+    /* Remove trailing newline */
+    if (line[nread - 1] == '\n')
+        line[nread - 1] = '\0';
+
+    return line;
 }
 
 /**
- * parse_line - Splits input into tokens.
- * @line: Input string to parse.
- * Return: Null-terminated array of tokens.
+ * parse_line - Parses a line into arguments
+ * @line: Line to parse
+ * Return: Array of arguments
  */
 char **parse_line(char *line)
 {
-	int bufsize = BUFSIZE, pos = 0;
-	char **tokens = malloc(bufsize * sizeof(char *));
-	char *token;
+    int bufsize = BUFSIZE, pos = 0;
+    char **tokens = malloc(bufsize * sizeof(char *));
+    char *token;
 
-	if (!tokens)
-	{
-		perror("malloc");
-		exit(EXIT_FAILURE);
-	}
+    if (!tokens)
+    {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
 
-	token = strtok(line, DELIM);
-	while (token)
-	{
-		tokens[pos++] = token;
-		if (pos >= bufsize)
-		{
-			bufsize += BUFSIZE;
-			tokens = realloc(tokens, bufsize * sizeof(char *));
-			if (!tokens)
-			{
-				perror("realloc");
-				exit(EXIT_FAILURE);
-			}
-		}
-		token = strtok(NULL, DELIM);
-	}
-	tokens[pos] = NULL;
-	return (tokens);
+    token = strtok(line, DELIM);
+    while (token)
+    {
+        tokens[pos++] = token;
+        if (pos >= bufsize)
+        {
+            bufsize += BUFSIZE;
+            tokens = realloc(tokens, bufsize * sizeof(char *));
+            if (!tokens)
+            {
+                perror("realloc");
+                exit(EXIT_FAILURE);
+            }
+        }
+        token = strtok(NULL, DELIM);
+    }
+    tokens[pos] = NULL;
+    return tokens;
 }

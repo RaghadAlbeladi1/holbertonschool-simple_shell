9ae0e6c32b48e004632dcc53dd0be48f7a978d32
RaghadAlbeladi <Raghadz_5@hotmail.com>
2025-07-01 12:47:15 +0000

    updaaaaaated execute 340

--- execute.c
+++ execute.c
@@ -1,11 +1,26 @@
 #include "shell.h"
 #include <sys/stat.h>
 #include <sys/wait.h>
-#include <unistd.h>
 
 /**
- * execute - Executes a command with arguments
- * @args: Array of command and arguments
+ * _strcmp - Custom string comparison
+ * @s1: First string
+ * @s2: Second string
+ * Return: 0 if equal, difference otherwise
+ */
+int _strcmp(const char *s1, const char *s2)
+{
+    while (*s1 && *s2 && *s1 == *s2)
+    {
+        s1++;
+        s2++;
+    }
+    return (*s1 - *s2);
+}
+
+/**
+ * execute - Executes command with arguments
+ * @args: Array of arguments
  * Return: 1 to continue, 0 to exit
  */
 int execute(char **args)
@@ -15,7 +30,7 @@ int execute(char **args)
     struct stat st;
 
     if (args[0] == NULL)
-        return (1); /* Empty command */
+        return (1);
 
     /* Handle built-in commands */
     if (_strcmp(args[0], "exit") == 0)
@@ -29,14 +44,14 @@ int execute(char **args)
         return (1);
     }
 
-    /* Check if command exists */
+    /* Check command existence */
     if (stat(args[0], &st) == -1)
     {
         fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
         return (1);
     }
 
-    /* Check execute permissions */
+    /* Check execute permission */
     if (!(st.st_mode & S_IXUSR))
     {
         fprintf(stderr, "./hsh: 1: %s: Permission denied\n", args[0]);
@@ -46,7 +61,6 @@ int execute(char **args)
     pid = fork();
     if (pid == 0)
     {
-        /* Child process */
         if (execve(args[0], args, environ) == -1)
         {
             fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
@@ -55,14 +69,11 @@ int execute(char **args)
     }
     else if (pid < 0)
     {
-        /* Forking error */
         perror("./hsh");
     }
     else
     {
-        /* Parent process */
         waitpid(pid, &status, WUNTRACED);
-        fflush(stdout); /* Ensure output is flushed */
     }
 
     return (1);

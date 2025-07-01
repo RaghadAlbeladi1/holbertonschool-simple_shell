bb62fbc953e2dd232a9c3369edefd92dd4b7a33d
RaghadAlbeladi <Raghadz_5@hotmail.com>
2025-07-01 23:44:30 +0000

    Addddddddddddddddd

--- user_input.c
+++ user_input.c
@@ -1,60 +1,28 @@
 #include "shell.h"
 
-/**
- * read_line - Reads a line from stdin
- * Return: Pointer to the line
- */
-char *read_line(void)
-{
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t nread;
-
-    nread = getline(&line, &len, stdin);
-    if (nread == -1)
-    {
-        free(line);
-        return NULL;
-    }
-
-    /* Remove trailing newline */
-    if (line[nread - 1] == '\n')
-        line[nread - 1] = '\0';
-
-    return line;
-}
-
-/**
- * parse_line - Parses a line into arguments
- * @line: Line to parse
- * Return: Array of arguments
- */
-char **parse_line(char *line)
-{
+char **parse_line(char *line) {
     int bufsize = BUFSIZE, pos = 0;
     char **tokens = malloc(bufsize * sizeof(char *));
-    char *token;
-
-    if (!tokens)
-    {
+    if (!tokens) {
         perror("malloc");
         exit(EXIT_FAILURE);
     }
 
-    token = strtok(line, DELIM);
-    while (token)
-    {
+    char *token = strtok(line, DELIM);
+    while (token) {
         tokens[pos++] = token;
-        if (pos >= bufsize)
-        {
+
+        if (pos >= bufsize) {
             bufsize += BUFSIZE;
-            tokens = realloc(tokens, bufsize * sizeof(char *));
-            if (!tokens)
-            {
+            char **new_tokens = realloc(tokens, bufsize * sizeof(char *));
+            if (!new_tokens) {
                 perror("realloc");
+                free(tokens);
                 exit(EXIT_FAILURE);
             }
+            tokens = new_tokens;
         }
+
         token = strtok(NULL, DELIM);
     }
     tokens[pos] = NULL;

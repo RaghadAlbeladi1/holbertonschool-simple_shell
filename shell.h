1b0c2db27e984d4578ada7ba785bb7968023c918
Bushra2252 <10949@holbertonstudents.com>
2025-07-01 10:44:25 +0000

    add header for exit,env

--- shell.h
+++ shell.h
@@ -1,25 +1,17 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-/* Standard headers */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
-#include <sys/types.h>
+#include <string.h>
 #include <sys/wait.h>
+#include <sys/types.h>
+#include <errno.h>
 
-/* Function Prototypes */
-void display_prompt(void);
-char *get_input(void);
-char **tokenize_input(char *line);
-int shell_exit(void);
-int shell_env(void);
-void execute_command(char **args, char **env);
-char *find_command(char *command, char **env);
+#define BUFSIZE 64
+#define DELIM " \t\n"
 
-
-/* Environment */
 extern char **environ;
 
 /* Function prototypes */
@@ -28,5 +20,8 @@ char *read_line(void);
 char **parse_line(char *line);
 int execute(char **args);
 void print_prompt(void);
+int shell_exit(void);
+int shell_env(void);
 
 #endif /* SHELL_H */
+

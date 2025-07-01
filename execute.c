800e34eda8c7a1d8527abf81dfda36cc334145ea
RaghadAlbeladi <Raghadz_5@hotmail.com>
2025-07-01 12:59:28 +0000

    add execute file updated 400

--- execute.c
+++ execute.c
@@ -2,28 +2,23 @@
 #include <sys/stat.h>
 #include <sys/wait.h>
 
-/**
- * _strcmp - Custom string comparison
- * @s1: First string
- * @s2: Second string
- * Return: 0 if equal, difference otherwise
- */
-int _strcmp(const char *s1, const char *s2)
+/* دالة مقارنة النصوص (بدون استخدام strcmp من string.h) */
+static int custom_strcmp(const char *s1, const char *s2)
 {
-    while (*s1 && *s2 && *s1 == *s2)
+    while (*s1 && (*s1 == *s2))
     {
         s1++;
         s2++;
     }
-    return (*s1 - *s2);
+    return *(unsigned char *)s1 - *(unsigned char *)s2;
 }
 
 /**
- * execute - Executes command with arguments
- * @args: Array of arguments
- * Return: 1 to continue, 0 to exit
+ * execute_command - تنفيذ الأوامر (بديل آمن لـ execute)
+ * @args: مصفوفة الأوامر والوسائط
+ * Return: 1 لمواصلة التنفيذ، 0 للخروج
  */
-int execute(char **args)
+int execute_command(char **args)
 {
     pid_t pid;
     int status;
@@ -32,11 +27,11 @@ int execute(char **args)
     if (args[0] == NULL)
         return (1);
 
-    /* Handle built-in commands */
-    if (_strcmp(args[0], "exit") == 0)
+    /* معالجة الأوامر المدمجة */
+    if (custom_strcmp(args[0], "exit") == 0)
         return (0);
 
-    if (_strcmp(args[0], "env") == 0)
+    if (custom_strcmp(args[0], "env") == 0)
     {
         char **env = environ;
         while (*env)
@@ -44,23 +39,25 @@ int execute(char **args)
         return (1);
     }
 
-    /* Check command existence */
+    /* التحقق من وجود الملف */
     if (stat(args[0], &st) == -1)
     {
         fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
         return (1);
     }
 
-    /* Check execute permission */
+    /* التحقق من صلاحيات التنفيذ */
     if (!(st.st_mode & S_IXUSR))
     {
         fprintf(stderr, "./hsh: 1: %s: Permission denied\n", args[0]);
         return (1);
     }
 
+    /* إنشاء عملية جديدة */
     pid = fork();
     if (pid == 0)
     {
+        /* عملية الابن */
         if (execve(args[0], args, environ) == -1)
         {
             fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
@@ -69,10 +66,12 @@ int execute(char **args)
     }
     else if (pid < 0)
     {
+        /* خطأ في fork */
         perror("./hsh");
     }
     else
     {
+        /* عملية الأب */
         waitpid(pid, &status, WUNTRACED);
     }
 

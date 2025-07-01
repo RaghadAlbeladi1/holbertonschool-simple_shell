96bb1612c0e4b8523d51b0241743322d5762cfc9
RaghadAlbeladi <Raghadz_5@hotmail.com>
2025-07-01 13:11:20 +0000

    add header file 411

--- shell.h
+++ shell.h
@@ -3,11 +3,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/wait.h>
-#include <sys/types.h>
-#include <errno.h>
 #include <sys/stat.h>
 
 #define BUFSIZE 64
@@ -15,27 +13,11 @@
 
 extern char **environ;
 
-/* Main Functions */
+/* Function prototypes */
+int execute(char **args);
 void shell_loop(void);
 char *read_line(void);
 char **parse_line(char *line);
-int execute(char **args);
 void print_prompt(void);
-int execute_command(char **args);
-
-/* Command Execution */
-int execute_external(char **args, char **env);
-char *find_path(char *command, char **env);
-
-/* Built-in Commands */
-int shell_exit(char **args, char **env);
-int shell_env(char **args, char **env);
-int shell_cd(char **args, char **env);
-
-/* Helper Functions */
-int _strcmp(const char *s1, const char *s2);
-int _atoi(char *s);
-void free_tokens(char **tokens);
-void _puts(char *str);
 
 #endif /* SHELL_H */

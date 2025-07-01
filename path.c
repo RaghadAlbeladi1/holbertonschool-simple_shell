a53bf359139b56c42bc60aa7743c94359242cfc3
RaghadAlbeladi <Raghadz_5@hotmail.com>
2025-07-01 22:51:12 +0000

    addddddd

--- path.c
+++ path.c
@@ -10,28 +10,16 @@ char *find_in_path(char *cmd)
     char *path, *path_copy, *dir, *full_path;
     int cmd_len, dir_len;
 
-    printf("=== DEBUG START: find_in_path() ===\n");
-    printf("DEBUG: Command to find: '%s'\n", cmd);
-
-    if (!cmd) {
-        printf("DEBUG: Null command received\n");
+    if (!cmd)
         return NULL;
-    }
 
     /* Check if command exists in current directory */
-    printf("DEBUG: Checking current directory for command...\n");
-    if (access(cmd, X_OK) == 0) {
-        printf("DEBUG: Found in current directory: '%s'\n", cmd);
+    if (access(cmd, X_OK) == 0)
         return strdup(cmd);
-    }
 
     path = getenv("PATH");
-    printf("DEBUG: PATH variable: '%s'\n", path ? path : "(null)");
-
-    if (!path) {
-        fprintf(stderr, "DEBUG: PATH environment variable not set\n");
+    if (!path)
         return NULL;
-    }
 
     path_copy = strdup(path);
     cmd_len = strlen(cmd);
@@ -39,34 +27,26 @@ char *find_in_path(char *cmd)
 
     while (dir)
     {
-        printf("DEBUG: Checking directory: '%s'\n", dir);
-        
         dir_len = strlen(dir);
         full_path = malloc(dir_len + cmd_len + 2);
         if (!full_path)
         {
-            printf("DEBUG: Memory allocation failed\n");
             free(path_copy);
             return NULL;
         }
 
         sprintf(full_path, "%s/%s", dir, cmd);
-        printf("DEBUG: Trying path: '%s'\n", full_path);
 
         if (access(full_path, X_OK) == 0)
         {
-            printf("DEBUG: Found at: '%s'\n", full_path);
             free(path_copy);
             return full_path;
         }
 
-        printf("DEBUG: Not found in: '%s'\n", full_path);
         free(full_path);
         dir = strtok(NULL, ":");
     }
 
-    printf("DEBUG: Command not found in any PATH directory\n");
     free(path_copy);
-    printf("=== DEBUG END: find_in_path() ===\n");
     return NULL;
 }

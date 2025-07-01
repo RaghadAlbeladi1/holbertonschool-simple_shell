f0ca36841e2a6d0faf2b2f6fabf42c86b59b906a
RaghadAlbeladi <Raghadz_5@hotmail.com>
2025-07-01 13:09:19 +0000

    add shell.c Raghad

--- shell.c
+++ shell.c
@@ -1,67 +1,11 @@
 #include "shell.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/wait.h>
 
-
-extern char **environ;
-
-/**
- * execute - Executes a command with arguments.
- * @args: Null-terminated array of arguments.
- * Return: 1 to continue, 0 to exit.
- */
-int execute(char **args)
-{
-    pid_t pid;
-    int status;
-
-    if (!args[0])
-        return (1);
-    if (strcmp(args[0], "exit") == 0)
-        return (0);
-    if (strcmp(args[0], "env") == 0)
-    {
-        char **env = environ;
-        while (*env)
-            printf("%s\n", *env++);
-        return (1);
-    }
-    pid = fork();
-    if (pid == 0)
-    {
-        if (execve(args[0], args, environ) == -1)
-        {
-            fprintf(stderr, "./shell: No such file or directory\n");
-            exit(EXIT_FAILURE);
-        }
-    }
-    else if (pid < 0)
-    {
-        perror("fork");
-    }
-    else
-    {
-        waitpid(pid, &status, 0);
-    }
-    return (1);
-}
-
-/**
- * main - Entry point for simple shell.
- * Return: Always EXIT_SUCCESS.
- */
 int main(void)
 {
     shell_loop();
     return (EXIT_SUCCESS);
 }
 
-/**
- * shell_loop - Main shell processing loop (REPL).
- */
 void shell_loop(void)
 {
     char *line = NULL;
@@ -78,13 +22,13 @@ void shell_loop(void)
                 write(STDOUT_FILENO, "\n", 1);
             break;
         }
+
         args = parse_line(line);
         if (args)
         {
-            status = execute(args);
+            status = execute(args);  // هذه ستستخدم الدالة من execute.c
             free(args);
         }
         free(line);
     }
-
 }

1c69d022b433204d2fc1789e7300e9bc99686e7e
RaghadAlbeladi <Raghadz_5@hotmail.com>
2025-07-01 23:28:53 +0000

    add

--- path.c
+++ path.c
@@ -1,27 +1,43 @@
 #include "shell.h"
 
 /**
- * find_in_path - Finds a command in PATH
+ * _getpath - Gets PATH from environ
+ * Return: PATH string or NULL
+ */
+char *_getpath(void)
+{
+    char **env = environ;
+    while (*env)
+    {
+        if (strncmp(*env, "PATH=", 5) == 0)
+            return (*env + 5);
+        env++;
+    }
+    return NULL;
+}
+
+/**
+ * find_in_path - Finds command in PATH
  * @cmd: Command to find
  * Return: Full path if found, NULL otherwise
  */
 char *find_in_path(char *cmd)
 {
     char *path, *path_copy, *dir, *full_path;
-    int cmd_len, dir_len;
+    size_t dir_len, cmd_len;
 
-    if (!cmd)
-        return NULL;
+    if (!cmd || !*cmd) return NULL;
 
-    /* Check if command exists in current directory */
+    /* Check current directory first */
     if (access(cmd, X_OK) == 0)
         return strdup(cmd);
 
-    path = getenv("PATH");
-    if (!path)
-        return NULL;
+    path = _getpath();
+    if (!path) return NULL;
 
     path_copy = strdup(path);
+    if (!path_copy) return NULL;
+
     cmd_len = strlen(cmd);
     dir = strtok(path_copy, ":");
 
@@ -36,7 +52,6 @@ char *find_in_path(char *cmd)
         }
 
         sprintf(full_path, "%s/%s", dir, cmd);
-
         if (access(full_path, X_OK) == 0)
         {
             free(path_copy);

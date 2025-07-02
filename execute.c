cf956b2b95f0df44378d9735a4efa589d113391e
Bushra2252 <10949@holbertonstudents.com>
2025-07-02 20:17:17 +0000

    update execute.c

--- execute.c
+++ execute.c
@@ -4,11 +4,7 @@
  * execute_command - Executes a shell command using fork and execvp
  * @command: The command string to execute
  *
- * Return: Exit status of the executed command:
- *         - 127 for command not found
- *         - 126 for permission denied
- *         - Actual status from executed command
- *         - 1 for other errors
+ * Return: Exit status of the executed command, or 1 on error
  */
 int execute_command(const char *command)
 {
@@ -19,9 +15,6 @@ int execute_command(const char *command)
     int arg_count = 0;
     char *token;
 
-    if (command == NULL || *command == '\0')
-        return (0);
-
     child_pid = fork();
     if (child_pid == -1)
     {
@@ -30,7 +23,6 @@ int execute_command(const char *command)
     }
     else if (child_pid == 0)
     {
-
         strncpy(command_copy, command, sizeof(command_copy) - 1);
         command_copy[sizeof(command_copy) - 1] = '\0';
 
@@ -42,35 +34,16 @@ int execute_command(const char *command)
         }
         args[arg_count] = NULL;
 
-
-        if (access(args[0], F_OK) == -1)
-        {
-            print_message(args[0]);
-            print_message(": command not found\n");
-            exit(127);
-        }
-        else if (access(args[0], X_OK) == -1)
-        {
-            print_message(args[0]);
-            print_message(": Permission denied\n");
-            exit(126);
-        }
-
         execvp(args[0], args);
-
-        perror(args[0]);
+        print_message("Error executing command.\n");
         exit(127);
     }
     else
     {
-
         waitpid(child_pid, &status, 0);
-
         if (WIFEXITED(status))
             return (WEXITSTATUS(status));
-        else if (WIFSIGNALED(status))
-            return (128 + WTERMSIG(status));
-        else
-            return (1);
+        return (1);
     }
 }
+

a0d5ebb8ad6a9fa1c61a433a8770c11ba2b587d4
RaghadAlbeladi <Raghadz_5@hotmail.com>
2025-07-02 00:02:58 +0000

    Adddd

--- user_input.c
+++ user_input.c
@@ -1,31 +1,32 @@
 #include "shell.h"
 
-/**
- * parse_line - Splits a string into tokens (must be freed by caller)
- * @line: The input string to split
- * Return: Array of tokens, or NULL on failure
- */
 char **parse_line(char *line)
 {
-    if (!line || !*line) {
-        return NULL;
-    }
-
     int bufsize = BUFSIZE, pos = 0;
-    char **tokens = malloc(bufsize * sizeof(char *));
-    if (!tokens) {
+    char **tokens, **new_tokens;
+    char *token;
+
+    if (!line || !*line)
+        return NULL;
+
+    tokens = malloc(bufsize * sizeof(char *));
+    if (!tokens)
+    {
         perror("malloc failed");
         return NULL;
     }
 
-    char *token = strtok(line, DELIM);
-    while (token) {
+    token = strtok(line, DELIM);
+    while (token)
+    {
         tokens[pos++] = token;
 
-        if (pos >= bufsize) {
+        if (pos >= bufsize)
+        {
             bufsize += BUFSIZE;
-            char **new_tokens = realloc(tokens, bufsize * sizeof(char *));
-            if (!new_tokens) {
+            new_tokens = realloc(tokens, bufsize * sizeof(char *));
+            if (!new_tokens)
+            {
                 perror("realloc failed");
                 free(tokens);
                 return NULL;

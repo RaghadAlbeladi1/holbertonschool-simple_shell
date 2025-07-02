6f35c593e812bc93290cf3938babb21815bb17d3
Bushra2252 <10949@holbertonstudents.com>
2025-07-02 09:45:07 +0000

    Fix bags

--- builtins.c
+++ builtins.c
@@ -3,31 +3,27 @@
 /**
 * handle_builtins - Handles built-in commands
 * @args: Command arguments
-*
 * Return: 1 if builtin handled, 0 otherwise
 */
 int handle_builtins(char **args)
 {
 if (!args[0])
-return (0);
+return 0;
 
 if (strcmp(args[0], "exit") == 0)
 {
-printf("OK\n");
-free_resources();
 exit(EXIT_SUCCESS);
 }
 else if (strcmp(args[0], "env") == 0)
 {
 char **env = environ;
-
 while (*env)
 {
 printf("%s\n", *env);
 env++;
 }
-return (1);
+return 1;
 }
 
-return (0);
+return 0;
 }

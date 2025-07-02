461a11c3c777fc19fa6407ad6a616587b7e695fe
RaghadAlbeladi <Raghadz_5@hotmail.com>
2025-07-02 00:55:28 +0000

    lllll

--- execute.c
+++ execute.c
@@ -1,37 +1,42 @@
 #include "shell.h"
 
-int execute(char **args) {
+/**
+ * execute - Execute a command
+ * @args: Array of arguments
+ * Return: 1 to continue, 0 to exit
+ */
+int execute(char **args)
+{
     pid_t pid;
     int status;
-    char *full_path = NULL;
 
-    if (!args || !args[0]) return 1;
-
-    if (handle_builtins(args)) return 1;
-
-    if (strchr(args[0], '/') == NULL) {
-        full_path = find_in_path(args[0]);
-        if (!full_path) {
-            fprintf(stderr, "%s: 1: %s: not found\n", "hsh", args[0]);
-            return 1;
-        }
-        args[0] = full_path;
-    }
-
-    pid = fork();
-    if (pid == 0) {
-        execve(args[0], args, environ);
-        perror("execve failed");
-        if (full_path && full_path != args[0]) free(full_path);
-        _exit(EXIT_FAILURE);
-    } else if (pid < 0) {
-        perror("fork failed");
-        if (full_path && full_path != args[0]) free(full_path);
+    if (args[0] == NULL)
         return 1;
-    } else {
-        waitpid(pid, &status, 0);
+
+    /* Handle built-in commands */
+    if (handle_builtins(args))
+        return 1;
+
+    /* Execute external command */
+    pid = fork();
+    if (pid == 0)
+    {
+        if (execvp(args[0], args) == -1)
+        {
+            perror("hsh");
+            exit(EXIT_FAILURE);
+        }
+    }
+    else if (pid < 0)
+    {
+        perror("hsh");
+    }
+    else
+    {
+        do {
+            waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
     }
 
-    if (full_path && full_path != args[0]) free(full_path);
     return 1;
 }

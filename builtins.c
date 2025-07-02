596feb581eb56d7f04db7fff82ba653cad39d82c
RaghadAlbeladi <Raghadz_5@hotmail.com>
2025-07-02 00:56:59 +0000

    add

--- builtins.c
+++ builtins.c
@@ -1,29 +1,44 @@
 #include "shell.h"
 
 /**
- * handle_builtins - Handles built-in commands
- * @args: Command arguments
- * Return: 1 if builtin handled, 0 otherwise
+ * handle_builtins - Handle built-in commands
+ * @args: Array of arguments
+ * Return: 1 if built-in found, 0 otherwise
  */
 int handle_builtins(char **args)
 {
-    if (!args[0])
-        return 0;
-
     if (strcmp(args[0], "exit") == 0)
     {
+        free_args(args);
         exit(EXIT_SUCCESS);
     }
-    else if (strcmp(args[0], "env") == 0)
+    else if (strcmp(args[0], "echo") == 0)
     {
-        char **env = environ;
-        while (*env)
+        for (int i = 1; args[i] != NULL; i++)
         {
-            printf("%s\n", *env);
-            env++;
+            printf("%s", args[i]);
+            if (args[i+1] != NULL)
+                printf(" ");
         }
+        printf("\n");
         return 1;
     }
+    /* Add more built-ins here */
 
     return 0;
+}
+
+/**
+ * free_args - Free memory allocated for arguments
+ * @args: Array of arguments to free
+ */
+void free_args(char **args)
+{
+    if (args == NULL)
+        return;
+
+    for (int i = 0; args[i] != NULL; i++)
+        free(args[i]);
+
+    free(args);
 }

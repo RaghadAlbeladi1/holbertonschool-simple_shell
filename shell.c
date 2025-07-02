97ed57d523dec133955180b1ea0cf6a2f9ce2528
Bushra2252 <10949@holbertonstudents.com>
2025-07-02 18:33:28 +0000

    update shell.c

--- shell.c
+++ shell.c
@@ -1,15 +1,22 @@
 #include "shell.h"
-/*
- *
+
+/**
+ * main - Entry point of the shell
+ * Return: Always 0 on success
  */
+int main(void)
+{
+	char command[120];
 
-int main(void) {
-    char command[120];
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			display_prompt();
 
-    while (true) {
-        display_prompt();
-        read_command(command, sizeof(command));
-        execute_command(command);
-  }
-    return 0;
+		read_command(command, sizeof(command));
+		execute_command(command);
+	}
+
+	return (0);
 }
+

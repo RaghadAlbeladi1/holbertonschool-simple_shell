1ffcbe96c1917ea5c8f72619b8dd9e34059ecc75
RaghadAlbeladi <Raghadz_5@hotmail.com>
2025-07-02 01:40:52 +0000

    last5

--- path.c
+++ path.c
@@ -1,49 +1,37 @@
 #include "shell.h"
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
+/**
+ * find_in_path - Finds a command in PATH
+ * @cmd: Command to find
+ * Return: Full path or NULL
+ */
 char *find_in_path(char *cmd)
 {
-    char *path, *path_copy, *dir, *full_path = NULL;
-    size_t dir_len, cmd_len;
+    char *path = _getpath();
+    char *dir, *full_path;
+    static char buffer[1024];
 
-    if (!cmd || !*cmd)
-        return NULL;
-
-    /* Check current directory first */
-    if (access(cmd, X_OK) == 0)
-        return strdup(cmd);
-
-    path = _getpath();
     if (!path)
         return NULL;
 
-    path_copy = strdup(path);
-    if (!path_copy)
-        return NULL;
-
-    cmd_len = strlen(cmd);
-    dir = strtok(path_copy, ":");
+    /* Make copy of PATH string */
+    path = strdup(path);
+    dir = strtok(path, ":");
 
     while (dir)
     {
-        dir_len = strlen(dir);
-        full_path = malloc(dir_len + cmd_len + 2);
-        if (!full_path)
+        snprintf(buffer, sizeof(buffer), "%s/%s", dir, cmd);
+        if (access(buffer, X_OK) == 0)
         {
-            free(path_copy);
-            return NULL;
+            free(path);
+            return buffer;
         }
-
-        sprintf(full_path, "%s/%s", dir, cmd);
-        if (access(full_path, X_OK) == 0)
-        {
-            free(path_copy);
-            return full_path;
-        }
-
-        free(full_path);
         dir = strtok(NULL, ":");
     }
 
-    free(path_copy);
+    free(path);
     return NULL;
 }

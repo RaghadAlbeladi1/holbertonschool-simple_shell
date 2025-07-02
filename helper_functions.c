6997b31f06b255eecfb050ef3496ec56f852de94
RaghadAlbeladi <Raghadz_5@hotmail.com>
2025-07-02 00:21:55 +0000

    end endd

--- helper_functions.c
+++ helper_functions.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+/* READ */
 char *read_line(void)
 {
     char *line = NULL;
@@ -17,6 +17,7 @@ char *read_line(void)
     return line;
 }
 
+/* READ */
 char *_getpath(void)
 {
     char **env = environ;

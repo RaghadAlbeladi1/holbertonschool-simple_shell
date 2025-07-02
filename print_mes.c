de0b35b6b18b42cf6a37688f625fc7bf78a4a366
Bushra2252 <10949@holbertonstudents.com>
2025-07-02 18:36:24 +0000

    update print_mes

--- print_mes.c
+++ print_mes.c
@@ -1,5 +1,12 @@
 #include "shell.h"
 
-void print_message(const char *message) {
-    write(STDOUT_FILENO, message, strlen(message));
+/**
+ * print_message - Prints a message to stdout
+ * @message: The message to print
+ */
+void print_message(const char *message)
+{
+	if (message)
+		write(STDOUT_FILENO, message, strlen(message));
 }
+

457924596ee31a3ee0d3a5b00f3684c7e6e0a5f9
Bushra2252 <10949@holbertonstudents.com>
2025-07-02 17:59:33 +0000

    update execute.c

---
+++ execute.c
@@ -0,0 +1,35 @@
+#include "shell.h"
+
+
+
+void execute_command(const char *command) {
+    pid_t child_pid = fork();
+
+    if (child_pid == -1) {
+        print_mes("Error forking process.\n");
+        exit(EXIT_FAILURE);
+    } else if (child_pid == 0) {
+
+
+
+        char *args[128];
+        int arg_count = 0;
+
+        char *token = strtok((char *)command, " ");
+        while (token != NULL) {
+            args[arg_count++] = token;
+            token = strtok(NULL, " ");
+        }
+        args[arg_count] = NULL;
+
+
+        execvp(args[0], args);
+
+
+        print_mes("Error executing command.\n");
+        exit(EXIT_FAILURE);
+    } else {
+
+        wait(NULL);
+    }
+}

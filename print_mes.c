b9db232789ce1421e1a62495ff17458d09df8979
Bushra2252 <10949@holbertonstudents.com>
2025-07-02 19:09:19 +0000

    update print_mes.c

--- print_mes.c
+++ print_mes.c
@@ -1,12 +1,15 @@
-#include "shell.h"
+#include <unistd.h>
+#include <string.h>
 
 /**
- * print_message - Prints a message to stdout
- * @message: The message to print
+ * print_message - Prints a message to stdout, optionally with newline
+ * @msg: Message to print
+ * @newline: If non-zero, print a newline after the message
  */
-void print_message(const char *message)
+void print_message(const char *msg, int newline)
 {
-	if (message)
-		write(STDOUT_FILENO, message, strlen(message));
+    write(STDOUT_FILENO, msg, strlen(msg));
+    if (newline)
+        write(STDOUT_FILENO, "\n", 1);
 }
 

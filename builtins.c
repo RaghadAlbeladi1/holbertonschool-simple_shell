9786a44a72c9e052b16b9bfe0febc856ef427536
RaghadAlbeladi <Raghadz_5@hotmail.com>
2025-07-02 01:28:45 +0000

    when we finish

--- builtins.c
+++ builtins.c
@@ -4,28 +4,23 @@
 #include <stdio.h>
 
 /**
- * handle_builtins - Handles built-in shell commands
- * @args: Array of command arguments
+ * handle_builtins - Execute built-in shell commands
+ * @args: Argument array
  * Return: 1 if built-in handled, 0 otherwise
  */
 int handle_builtins(char **args)
 {
-    if (!args[0])
+    if (!args || !args[0])
         return 0;
 
-    if (strcmp(args[0], "exit") == 0)
-    {
+    if (strcmp(args[0], "exit") == 0) {
         exit(EXIT_SUCCESS);
     }
-    else if (strcmp(args[0], "echo") == 0)
-    {
+    else if (strcmp(args[0], "echo") == 0) {
         int i = 1;
-        while (args[i])
-        {
+        while (args[i]) {
             printf("%s", args[i]);
-            if (args[i+1])
-                printf(" ");
-            i++;
+            if (args[++i]) printf(" ");
         }
         printf("\n");
         return 1;
@@ -34,19 +29,14 @@ int handle_builtins(char **args)
 }
 
 /**
- * free_args - Frees memory allocated for arguments
- * @args: Array of arguments to free
+ * free_args - Free argument array memory
+ * @args: Argument array to free
  */
 void free_args(char **args)
 {
-    if (!args)
-        return;
-
+    if (!args) return;
+    
     int i = 0;
-    while (args[i])
-    {
-        free(args[i]);
-        i++;
-    }
+    while (args[i]) free(args[i++]);
     free(args);
 }

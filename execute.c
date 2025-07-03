7ab3f143e3a338f7261bdbc95be730fba8cc7caa
Bushra2252 <10949@holbertonstudents.com>
2025-07-03 03:24:04 +0000

    update

--- execute.c
+++ execute.c
@@ -1,76 +1,55 @@
 #include "shell.h"
 
 /**
- * execute_command - Executes a shell command using fork and execvp
- * @command: The command string to execute
+ * execute_command - Executes a command using fork and exec
+ * @command: The command to execute
  *
- * Return: Exit status of the executed command:
- *         - 127 for command not found
- *         - 126 for permission denied
- *         - Actual status from executed command
- *         - 1 for other errors
+ * Return: Exit status of the command
  */
 int execute_command(const char *command)
 {
-    pid_t child_pid;
+    pid_t pid;
     int status;
-    char *args[128];
-    char command_copy[1024];
-    int arg_count = 0;
+    char *args[MAX_ARGS];
     char *token;
+    int i = 0;
+    char *cmd_copy = strdup(command);
 
-    if (command == NULL || *command == '\0')
-        return (0);
+    if (!cmd_copy)
+        return (-1);
 
-    child_pid = fork();
-    if (child_pid == -1)
+    token = strtok(cmd_copy, " ");
+    while (token && i < MAX_ARGS - 1)
+    {
+        args[i++] = token;
+        token = strtok(NULL, " ");
+    }
+    args[i] = NULL;
+
+    pid = fork();
+    if (pid == -1)
     {
         print_message("Error forking process.\n");
-        return (1);
+        free(cmd_copy);
+        return (-1);
     }
-    else if (child_pid == 0)
+
+    if (pid == 0)
     {
-
-        strncpy(command_copy, command, sizeof(command_copy) - 1);
-        command_copy[sizeof(command_copy) - 1] = '\0';
-
-        token = strtok(command_copy, " ");
-        while (token != NULL && arg_count < 127)
-        {
-            args[arg_count++] = token;
-            token = strtok(NULL, " ");
-        }
-        args[arg_count] = NULL;
-
-
-        if (access(args[0], F_OK) == -1)
+        if (execvp(args[0], args) == -1)
         {
             print_message(args[0]);
             print_message(": command not found\n");
+            free(cmd_copy);
             exit(127);
         }
-        else if (access(args[0], X_OK) == -1)
-        {
-            print_message(args[0]);
-            print_message(": Permission denied\n");
-            exit(126);
-        }
-
-        execvp(args[0], args);
-
-        perror(args[0]);
-        exit(127);
     }
-    else
-    {
 
-        waitpid(child_pid, &status, 0);
+    waitpid(pid, &status, 0);
+    free(cmd_copy);
 
-        if (WIFEXITED(status))
-            return (WEXITSTATUS(status));
-        else if (WIFSIGNALED(status))
-            return (128 + WTERMSIG(status));
-        else
-            return (1);
-    }
+    if (WIFEXITED(status))
+        return (WEXITSTATUS(status));
+
+    return (-1);
 }

5de731c76b2684b3bfb9a869a71f3409b50f6b36
Bushra2252 <10949@holbertonstudents.com>
2025-07-03 03:14:52 +0000

    update

--- builtins.c
+++ builtins.c
@@ -1,28 +1,39 @@
 #include "shell.h"
 
 /**
- * handle_builtin - Handles built-in shell commands
- * @command: The command to check
+ * handle_builtin - Check and execute built-in commands
+ * @command: Input command string
  *
- * Return: 1 if builtin handled, -1 if exit, 0 otherwise
+ * Return: -1 if exit was called, 1 if builtin was handled, 0 otherwise
  */
 int handle_builtin(char *command)
 {
     char *cmd = strtok(command, " ");
-    int i = 0;
-    extern char **environ;
-
-    if (!cmd)
-        return (0);
+    char *arg = strtok(NULL, " ");
 
     if (strcmp(cmd, "exit") == 0)
-        return (-1);
-
-    if (strcmp(cmd, "env") == 0)
     {
+        if (arg == NULL)
+        {
+
+            return (-1);
+        }
+        else
+        {
+
+            int status = atoi(arg);
+            exit(status);
+        }
+    }
+    else if (strcmp(cmd, "env") == 0)
+    {
+        extern char **environ;
+        int i = 0;
+
         while (environ[i])
         {
-            print_message(environ[i], 1);
+            print_message(environ[i]);
+            print_message("\n");
             i++;
         }
         return (1);

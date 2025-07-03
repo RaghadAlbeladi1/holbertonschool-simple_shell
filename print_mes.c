567b1531c9230abd6e54238dfdc4a91125fd3d26
Bushra2252 <10949@holbertonstudents.com>
2025-07-03 03:11:16 +0000

    update

--- print_mes.c
+++ print_mes.c
@@ -1,33 +1,15 @@
-#include "shell.h"
 #include <unistd.h>
 #include <string.h>
 
 /**
- * print_message - Prints a message to stdout with newline control
+ * print_message - Prints a message to stdout, optionally with newline
  * @msg: Message to print
- * @newline: Flag to add newline (1 = yes, 0 = no)
- *
- * Return: Number of bytes written, -1 on error
+ * @newline: If non-zero, print a newline after the message
  */
-ssize_t print_message(const char *msg, int newline)
+void print_message(const char *msg, int newline)
 {
-    ssize_t bytes = 0, ret;
-
-    if (!msg)
-        return (0);
-
-    ret = write(STDOUT_FILENO, msg, strlen(msg));
-    if (ret == -1)
-        return (-1);
-    bytes += ret;
-
+    write(STDOUT_FILENO, msg, strlen(msg));
     if (newline)
-    {
-        ret = write(STDOUT_FILENO, "\n", 1);
-        if (ret == -1)
-            return (-1);
-        bytes += ret;
-    }
-
-    return (bytes);
+        write(STDOUT_FILENO, "\n", 1);
 }
+
